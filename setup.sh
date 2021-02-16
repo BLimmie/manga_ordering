884bee4b28c9a2f7cf04513c0a451cfdba0a70ff
BLimmie <brian01.lim@gmail.com>
2021-02-15 22:00:30 -0800

    requirements.txt and setup script update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 pip3 install fugashi[unidic]
 python3 -m unidic download
+pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 pip3 install -r requirements.txt
