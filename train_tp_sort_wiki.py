8b646b22c46e444cb3e5146662a219380a694902
BLimmie <brian01.lim@gmail.com>
2021-03-08 20:27:39 -0800

    change from smoke

--- train_tp_sort_wiki.py
+++ train_tp_sort_wiki.py
@@ -3,19 +3,19 @@ from transformers import Trainer, TrainingArguments
 from data.dataloaders import PairwiseWikiData
 from models.tpsortmodels import base_order_model
 
-train_set = PairwiseWikiData("jawiki/japanese_wiki_smoke.json", divisions=10)
+train_set = PairwiseWikiData("jawiki/japanese_wiki_paragraphs.json", divisions=10)
 model = base_order_model()
 
 training_args = TrainingArguments(
     output_dir="ckpt",
     num_train_epochs=3,
-    per_device_train_batch_size=50,
+    per_device_train_batch_size=40,
     learning_rate=5e-6,
     warmup_steps=500,
     weight_decay=0.01,
     logging_dir='./logs',
-    logging_steps=len(train_set) // 100 + 1,
-    save_steps=len(train_set) // 100 + 1,
+    logging_steps=len(train_set) // 200 + 1,
+    save_steps=len(train_set) // 200 + 1,
     save_total_limit=2,
     dataloader_num_workers=4
 )
